64f1090789b2d9692176d0a9f79dcf419c6ca42e
asadkarim123 <45041410+asadkarim123@users.noreply.github.com>
2018-11-26 14:30:21 +0000

    Update slackNotifier.groovy

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -30,17 +30,17 @@ def call(String buildStatus = 'STARTED', String channel = '#alerts') {
 
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
-    color = 'YELLOW'
-    colorCode = '#FFFF00'
+    color = 'GREY'
+    colorCode = '#	#f0ffff'
   } else if (buildStatus == 'SUCCESSFUL') {
     color = 'GREEN'
-    colorCode = 'good'
+    colorCode = '#458b74'
   } else if (buildStatus == 'UNSTABLE') {
     color = 'YELLOW'
-    colorCode = 'warning'
+    colorCode = '	#ffd700'
   } else {
     color = 'RED'
-    colorCode = 'danger'
+    colorCode = '	#cd2626'
   }
 
   // get test results for slack message
